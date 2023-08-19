1daf3f4dae138b350653a8a6874ea89e978294c5
rz03 <tushar.code.inbox@gmail.com>
2023-08-19 18:51:33 +0530

    [feat] : Add dashboard page using shadcn

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,11 @@
+import Home from "./pages/Home";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline ">Hello world!</h1>;
+  return (
+    <div>
+      <Home />
+    </div>
+  );
 }
 
 export default App;
