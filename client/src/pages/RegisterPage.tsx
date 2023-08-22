242c2d87695f2c1c60554bc8a84878b4427d32d6
rz03 <tushar.code.inbox@gmail.com>
2023-08-22 21:49:48 +0530

    [style] : Added image to register and login pages

--- client/src/pages/RegisterPage.tsx
+++ client/src/pages/RegisterPage.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import { cn } from "../../lib/utils";
 import { buttonVariants } from "../../components/ui/button";
 import { RegisterForm } from "../components/RegisterForm";
-
+import backgroundImage from "../assets/plant.jpg";
 export default function AuthenticationPage() {
   return (
     <>
@@ -17,8 +17,14 @@ export default function AuthenticationPage() {
         >
           Login
         </Link>
-        <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
-          <div className="absolute inset-0 bg-zinc-900" />
+        <div
+          className="relative hidden h-full flex-col p-10 text-black dark:border-r lg:flex"
+          style={{
+            backgroundImage: `url(${backgroundImage})`,
+            backgroundSize: "cover",
+            backgroundPosition: "left",
+          }}
+        >
           <div className="relative z-20 flex items-center text-lg font-medium">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -32,17 +38,7 @@ export default function AuthenticationPage() {
             >
               <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
             </svg>
-            Acme Inc
-          </div>
-          <div className="relative z-20 mt-auto">
-            <blockquote className="space-y-2">
-              <p className="text-lg">
-                &ldquo;This library has saved me countless hours of work and
-                helped me deliver stunning designs to my clients faster than
-                ever before.&rdquo;
-              </p>
-              <footer className="text-sm">Sofia Davis</footer>
-            </blockquote>
+            Expenze
           </div>
         </div>
         <div className="lg:p-8">
