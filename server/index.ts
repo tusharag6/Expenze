8c2bf5f352c0c13d40abe98b246fa5ba3379bcce
rz03 <tushar.code.inbox@gmail.com>
2023-08-23 17:59:19 +0530

    [feat] : Set up prisma

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,26 @@
 import express, { Express, Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
 import dotenv from "dotenv";
-
 dotenv.config();
-
 const app: Express = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 8080;
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const user = await prisma.user.create({
+    data: { name: "kyle", email: "kyle@gmail.com" },
+  });
+  console.log(user);
+}
+
+main()
+  .catch((e) => {
+    console.error(e.message);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express server is running ...");
