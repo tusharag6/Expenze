a1fb9b0297db10c973cbdab39313baed80357726
rz03 <tushar.code.inbox@gmail.com>
2023-09-10 23:23:50 +0530

    [fix]: fixed sidebar height

--- client/src/layout/Sidebar.tsx
+++ client/src/layout/Sidebar.tsx
@@ -10,7 +10,7 @@ type SidebarProps = {
 export function Sidebar({ className }: SidebarProps) {
   const [selected, setSelected] = useState("Dashboard");
   return (
-    <div className={cn("pb-12", className)}>
+    <div className={cn("pb-12 h-[100vh]", className)}>
       <div className="space-y-4 py-4">
         <div className="px-3 py-2">
           <h2 className="mb-2 px-4 text-lg font-semibold tracking-tight">
