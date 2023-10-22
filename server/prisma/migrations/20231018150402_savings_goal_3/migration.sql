03b0a0d0874abf9722d60d3a8b934b327280fc78
rz03 <tushar.code.inbox@gmail.com>
2023-10-22 19:58:19 +0530

    [feat]: made schema for bills & subscription feat

--- server/prisma/migrations/20231018150402_savings_goal_3/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `defaultTargetAmount` on table `MarketplaceGoal` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "MarketplaceGoal" ALTER COLUMN "defaultTargetAmount" SET NOT NULL;
---
+++ server/prisma/migrations/20231022142638_bills_and_subs_1/migration.sql
@@ -0,0 +1,248 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" TEXT NOT NULL DEFAULT 'Personal',
+    "verified" BOOLEAN NOT NULL DEFAULT false,
+    "verificationToken" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "PasswordResetToken" (
+    "id" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "expiresAt" TIMESTAMPTZ NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "PasswordResetToken_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Account" (
+    "id" TEXT NOT NULL,
+    "account_name" TEXT NOT NULL,
+    "account_number" TEXT NOT NULL,
+    "total_balance" DOUBLE PRECISION NOT NULL,
+    "user_id" TEXT NOT NULL,
+
+    CONSTRAINT "Account_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Transaction" (
+    "id" TEXT NOT NULL,
+    "date" TIMESTAMPTZ NOT NULL,
+    "amount" DOUBLE PRECISION NOT NULL,
+    "type" TEXT NOT NULL,
+    "budgetCategory" TEXT,
+    "description" TEXT,
+    "account_id" TEXT NOT NULL,
+    "budgetCategoryId" TEXT,
+
+    CONSTRAINT "Transaction_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "BudgetCategory" (
+    "id" TEXT NOT NULL,
+    "budgetCategory" TEXT NOT NULL,
+    "amount" DOUBLE PRECISION NOT NULL,
+    "user_id" TEXT NOT NULL,
+
+    CONSTRAINT "BudgetCategory_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "SavingsGoal" (
+    "id" TEXT NOT NULL,
+    "goalName" TEXT NOT NULL,
+    "goalDescription" TEXT,
+    "targetAmount" DOUBLE PRECISION NOT NULL,
+    "targetDate" TIMESTAMPTZ NOT NULL,
+    "currentBalance" DOUBLE PRECISION,
+    "isAchieved" BOOLEAN,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "SavingsGoal_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "SavingsContribution" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "goalId" TEXT NOT NULL,
+    "amount" DOUBLE PRECISION NOT NULL,
+    "contributionDate" TIMESTAMPTZ NOT NULL,
+
+    CONSTRAINT "SavingsContribution_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "IncomeAllocation" (
+    "id" TEXT NOT NULL,
+    "allocationPercentage" DOUBLE PRECISION NOT NULL,
+    "userId" TEXT NOT NULL,
+    "savingsGoalId" TEXT NOT NULL,
+
+    CONSTRAINT "IncomeAllocation_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "MarketplaceGoal" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "defaultTargetAmount" DOUBLE PRECISION NOT NULL,
+
+    CONSTRAINT "MarketplaceGoal_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Notification" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "message" TEXT NOT NULL,
+    "notificationDate" TIMESTAMPTZ NOT NULL,
+    "isRead" BOOLEAN NOT NULL,
+
+    CONSTRAINT "Notification_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Bill" (
+    "id" TEXT NOT NULL,
+    "billName" TEXT NOT NULL,
+    "dueDate" TIMESTAMPTZ NOT NULL,
+    "billAmout" DOUBLE PRECISION NOT NULL,
+    "isRecurring" BOOLEAN NOT NULL,
+    "interval" TEXT NOT NULL,
+    "category" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "Bill_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Subscription" (
+    "id" TEXT NOT NULL,
+    "subscriptionName" TEXT NOT NULL,
+    "startDate" TIMESTAMPTZ NOT NULL,
+    "endDate" TIMESTAMPTZ NOT NULL,
+    "monthlyCost" DOUBLE PRECISION NOT NULL,
+    "renewalDate" TIMESTAMPTZ NOT NULL,
+    "category" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "Subscription_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "BillPayments" (
+    "id" TEXT NOT NULL,
+    "paymentDate" TIMESTAMPTZ NOT NULL,
+    "paymentAmount" DOUBLE PRECISION NOT NULL,
+    "billId" TEXT NOT NULL,
+
+    CONSTRAINT "BillPayments_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Household" (
+    "id" TEXT NOT NULL,
+    "ownerUserId" TEXT NOT NULL,
+    "joiningId" TEXT NOT NULL,
+
+    CONSTRAINT "Household_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "HouseholdMember" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "householdId" TEXT NOT NULL,
+
+    CONSTRAINT "HouseholdMember_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "HouseholdAccount" (
+    "id" TEXT NOT NULL,
+    "accountId" TEXT NOT NULL,
+    "userId" TEXT,
+    "householdId" TEXT NOT NULL,
+
+    CONSTRAINT "HouseholdAccount_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "PasswordResetToken_token_key" ON "PasswordResetToken"("token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Household_ownerUserId_key" ON "Household"("ownerUserId");
+
+-- AddForeignKey
+ALTER TABLE "PasswordResetToken" ADD CONSTRAINT "PasswordResetToken_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Account" ADD CONSTRAINT "Account_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Transaction" ADD CONSTRAINT "Transaction_account_id_fkey" FOREIGN KEY ("account_id") REFERENCES "Account"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Transaction" ADD CONSTRAINT "Transaction_budgetCategoryId_fkey" FOREIGN KEY ("budgetCategoryId") REFERENCES "BudgetCategory"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "BudgetCategory" ADD CONSTRAINT "BudgetCategory_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "SavingsGoal" ADD CONSTRAINT "SavingsGoal_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "SavingsContribution" ADD CONSTRAINT "SavingsContribution_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "SavingsContribution" ADD CONSTRAINT "SavingsContribution_goalId_fkey" FOREIGN KEY ("goalId") REFERENCES "SavingsGoal"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "IncomeAllocation" ADD CONSTRAINT "IncomeAllocation_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "IncomeAllocation" ADD CONSTRAINT "IncomeAllocation_savingsGoalId_fkey" FOREIGN KEY ("savingsGoalId") REFERENCES "SavingsGoal"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Notification" ADD CONSTRAINT "Notification_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Bill" ADD CONSTRAINT "Bill_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Subscription" ADD CONSTRAINT "Subscription_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "BillPayments" ADD CONSTRAINT "BillPayments_billId_fkey" FOREIGN KEY ("billId") REFERENCES "Bill"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Household" ADD CONSTRAINT "Household_ownerUserId_fkey" FOREIGN KEY ("ownerUserId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "HouseholdMember" ADD CONSTRAINT "HouseholdMember_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "HouseholdMember" ADD CONSTRAINT "HouseholdMember_householdId_fkey" FOREIGN KEY ("householdId") REFERENCES "Household"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "HouseholdAccount" ADD CONSTRAINT "HouseholdAccount_accountId_fkey" FOREIGN KEY ("accountId") REFERENCES "Account"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "HouseholdAccount" ADD CONSTRAINT "HouseholdAccount_householdId_fkey" FOREIGN KEY ("householdId") REFERENCES "Household"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "HouseholdAccount" ADD CONSTRAINT "HouseholdAccount_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
