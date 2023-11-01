56d215e24222ca6c754ac945257f3651087cd88f
rz03 <tushar.code.inbox@gmail.com>
2023-11-01 21:38:10 +0530

    [refactor]: revert back to sidebar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,24 +3,28 @@ import AccountSwitcher from "./features/account/components/AccountSwitcher";
 import { Sidebar } from "./layout/Sidebar";
 import { UserNav } from "./features/profile/components/UserNav";
 import logo from "./assets/logo.png";
-import { MainNav } from "./layout/MainNav";
+import { Search } from "./components/Search";
 
 function App() {
   return (
-    <div className="md:flex-col md:flex col-span-3 lg:col-span-5 lg:border-l px-7 md:px-14">
-      {/* Navbar */}
-      <div className="border-b">
-        <div className="flex h-16 items-center px-7 md:px-14">
-          <img src={logo} alt="Expenze Logo" className=" w-40 my-4" />
-          <MainNav className="mx-6" />
-          <div className="ml-auto flex items-center space-x-4">
-            <AccountSwitcher />
-            <UserNav />
+    <div className="grid lg:grid-cols-6">
+      {/* Sidebar */}
+      <Sidebar className="hidden lg:block" />
+      <div className="md:flex-col md:flex col-span-3 lg:col-span-5 lg:border-l">
+        {/* Navbar */}
+        <div className="border-b">
+          <div className="flex h-16 items-center px-4 md:px-8">
+            <Search />
+            {/* <MainNav className="mx-6" /> */}
+            <div className="ml-auto flex items-center space-x-4">
+              <AccountSwitcher />
+              <UserNav />
+            </div>
           </div>
         </div>
+        {/* Renders the nested route components */}
+        <Outlet />
       </div>
-      {/* Renders the nested route components */}
-      <Outlet />
     </div>
   );
 }
