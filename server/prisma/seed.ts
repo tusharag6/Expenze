52fbf825b004aed22d29742598da2fc820e49ba1
rz03 <tushar.code.inbox@gmail.com>
2023-11-05 19:46:37 +0530

    changed seed data

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -5,7 +5,6 @@ const prisma = new PrismaClient();
 
 async function seed() {
   try {
-    // Create a user
     const hashedPassword = await bcrypt.hash("Demo@1234", 10);
 
     const user = await prisma.user.create({
@@ -20,45 +19,78 @@ async function seed() {
     // Create two accounts for the user
     const account1 = await prisma.account.create({
       data: {
-        account_name: "Account 1",
+        account_name: "HDFC Bank",
         account_number: "123456789",
-        total_balance: 1000.0,
+        total_balance: 10000.0,
         user_id: user.id,
       },
     });
 
     const account2 = await prisma.account.create({
       data: {
-        account_name: "Account 2",
+        account_name: "SBI Bank",
         account_number: "987654321",
-        total_balance: 2000.0,
+        total_balance: 20000.0,
         user_id: user.id,
       },
     });
 
-    // Create two budget categories
+    // Create budget categories
     const budgetCategory1 = await prisma.budgetCategory.create({
       data: {
-        budgetCategory: "Category 1",
-        amount: 500.0,
+        budgetCategory: "Housing",
+        amount: 2000.0,
         user_id: user.id,
       },
     });
 
     const budgetCategory2 = await prisma.budgetCategory.create({
       data: {
-        budgetCategory: "Category 2",
+        budgetCategory: "Education",
         amount: 800.0,
         user_id: user.id,
       },
     });
 
-    // Create five transactions in each budget category
-    for (let i = 1; i <= 5; i++) {
+    const budgetCategory3 = await prisma.budgetCategory.create({
+      data: {
+        budgetCategory: "Entertainment",
+        amount: 1000.0,
+        user_id: user.id,
+      },
+    });
+
+    const budgetCategory4 = await prisma.budgetCategory.create({
+      data: {
+        budgetCategory: "Food",
+        amount: 1500.0,
+        user_id: user.id,
+      },
+    });
+
+    const budgetCategory5 = await prisma.budgetCategory.create({
+      data: {
+        budgetCategory: "Transportation",
+        amount: 200.0,
+        user_id: user.id,
+      },
+    });
+
+    const budgetCategory6 = await prisma.budgetCategory.create({
+      data: {
+        budgetCategory: "Shopping",
+        amount: 600.0,
+        user_id: user.id,
+      },
+    });
+
+    // Create transactions for each budget category
+    for (let i = 1; i <= 3; i++) {
+      // Create expense transactions
       await prisma.transaction.create({
         data: {
           date: new Date(),
-          amount: 100.0 * i,
+          amount: Math.round(Math.random() * 200),
           type: "Expense",
           budgetCategoryId: budgetCategory1.id,
           account_id: account1.id,
@@ -68,13 +100,156 @@ async function seed() {
       await prisma.transaction.create({
         data: {
           date: new Date(),
-          amount: 200.0 * i,
+          amount: Math.round(Math.random() * 100),
+          type: "Expense",
+          budgetCategoryId: budgetCategory2.id,
+          account_id: account1.id,
+        },
+      });
+
+      await prisma.transaction.create({
+        data: {
+          date: new Date(),
+          amount: Math.round(Math.random() * 200),
+          type: "Expense",
+          budgetCategoryId: budgetCategory3.id,
+          account_id: account1.id,
+        },
+      });
+
+      await prisma.transaction.create({
+        data: {
+          date: new Date(),
+          amount: Math.round(Math.random() * 100),
+          type: "Expense",
+          budgetCategoryId: budgetCategory4.id,
+          account_id: account1.id,
+        },
+      });
+    }
+    for (let i = 1; i <= 2; i++) {
+      // Create expense transactions
+      await prisma.transaction.create({
+        data: {
+          date: new Date(),
+          amount: Math.round(Math.random() * 200),
+          type: "Expense",
+          budgetCategoryId: budgetCategory1.id,
+          account_id: account2.id,
+        },
+      });
+
+      await prisma.transaction.create({
+        data: {
+          date: new Date(),
+          amount: Math.round(Math.random() * 100),
           type: "Expense",
           budgetCategoryId: budgetCategory2.id,
           account_id: account2.id,
         },
       });
+
+      await prisma.transaction.create({
+        data: {
+          date: new Date(),
+          amount: Math.round(Math.random() * 200),
+          type: "Expense",
+          budgetCategoryId: budgetCategory3.id,
+          account_id: account2.id,
+        },
+      });
+
+      await prisma.transaction.create({
+        data: {
+          date: new Date(),
+          amount: Math.round(Math.random() * 100),
+          type: "Expense",
+          budgetCategoryId: budgetCategory4.id,
+          account_id: account2.id,
+        },
+      });
     }
+
+    // Create income transactions
+    await prisma.transaction.create({
+      data: {
+        date: new Date(),
+        amount: Math.round(Math.random() * 1000),
+        type: "Income",
+        description: "Salary",
+        account_id: account1.id,
+      },
+    });
+
+    await prisma.transaction.create({
+      data: {
+        date: new Date(),
+        amount: Math.round(Math.random() * 500),
+        type: "Income",
+        description: "Freelance Gig",
+        account_id: account2.id,
+      },
+    });
+
+    await prisma.transaction.create({
+      data: {
+        date: new Date(),
+        amount: Math.round(Math.random() * 300),
+        type: "Income",
+        description: "Scholarship",
+        account_id: account1.id,
+      },
+    });
+
+    await prisma.transaction.create({
+      data: {
+        date: new Date(),
+        amount: Math.round(Math.random() * 200),
+        type: "Income",
+        description: "Gift",
+        account_id: account2.id,
+      },
+    });
+
+    await prisma.transaction.create({
+      data: {
+        date: new Date(),
+        amount: Math.round(Math.random() * 1000),
+        type: "Income",
+        description: "Salary",
+        account_id: account2.id,
+      },
+    });
+
+    await prisma.transaction.create({
+      data: {
+        date: new Date(),
+        amount: Math.round(Math.random() * 500),
+        type: "Income",
+        description: "Freelance Gig",
+        account_id: account2.id,
+      },
+    });
+
+    await prisma.transaction.create({
+      data: {
+        date: new Date(),
+        amount: Math.round(Math.random() * 300),
+        type: "Income",
+        description: "Scholarship",
+        account_id: account2.id,
+      },
+    });
+
+    await prisma.transaction.create({
+      data: {
+        date: new Date(),
+        amount: Math.round(Math.random() * 200),
+        type: "Income",
+        description: "Gift",
+        account_id: account2.id,
+      },
+    });
 
     console.log("Seed data has been created.");
   } catch (error) {
