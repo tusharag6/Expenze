06fc2d3c470283f30ad67f9717b119e05f489a77
rz03 <tushar.code.inbox@gmail.com>
2023-11-06 10:55:09 +0530

    [style]: changed style of goal card

--- client/src/features/goals/components/MarketplaceGoals.tsx
+++ client/src/features/goals/components/MarketplaceGoals.tsx
@@ -6,27 +6,30 @@ const realGoals = [
   {
     id: 1,
     name: "Buy a New Car",
-    targetDate: "2023-12-31",
+    amount: 10000,
   },
   {
     id: 2,
     name: "Vacation in Hawaii",
-    targetDate: "2024-07-15",
+    amount: 5000,
   },
   {
     id: 3,
     name: "Renovate the House",
     targetDate: "2023-10-01",
+    amount: 20000,
   },
   {
     id: 4,
     name: "Start a Business",
     targetDate: "2024-06-30",
+    amount: 100000,
   },
   {
     id: 5,
     name: "Save for Retirement",
     targetDate: "2030-12-31",
+    amount: 1000000,
   },
 ];
 
--- client/src/features/goals/components/MarketplaceGoalsCard.tsx
+++ client/src/features/goals/components/MarketplaceGoalsCard.tsx
@@ -1,8 +1,15 @@
 import React from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogTrigger,
+} from "../../../../components/ui/dialog";
+import { FaThList } from "react-icons/fa";
+import { Button } from "../../../../components/ui/button";
 
 interface Goal {
   name: string;
-  targetDate: string;
+  amount: number;
 }
 
 interface GoalsProps extends React.HTMLAttributes<HTMLDivElement> {
@@ -14,22 +21,26 @@ interface GoalsProps extends React.HTMLAttributes<HTMLDivElement> {
 
 export function GoalsCard({ width, height, goals }: GoalsProps) {
   return (
-    <div className="space-y-3">
-      <div className="overflow-hidden rounded-md">
-        <img
-          src="https://images.unsplash.com/photo-1596120236172-231999844ade?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8dmFjYXRpb258ZW58MHx8MHx8fDA%3D"
-          alt="goals demo"
-          width={width}
-          height={height}
-          className="aspect-[3/4] object-cover w-[150px] h-[150px]"
-        />
-      </div>
-      <div className="space-y-1 text-sm">
-        <h3 className="font-medium leading-none">{goals.name}</h3>
-        <p className="text-xs text-muted-foreground text-slate-500">
-          {goals.targetDate}
-        </p>
-      </div>
-    </div>
+    <Dialog>
+      <DialogTrigger asChild>
+        <div className="border border-border rounded-md flex w-auto p-4 items-center justify-between gap-12 py-6">
+          <div className="flex items-center gap-4">
+            <span className="border border-border p-2 rounded-sm bg-muted">
+              <FaThList size="20" />
+            </span>
+            <div>
+              <div className="text-muted-foreground font-light">
+                {goals.name}
+              </div>
+              <div className="font-bold text-xl">${goals.amount}</div>
+            </div>
+          </div>
+          <div>
+            <Button variant={"outline"}>Add</Button>
+          </div>
+        </div>
+      </DialogTrigger>
+      <DialogContent></DialogContent>
+    </Dialog>
   );
 }
